dee4efc79887181a02ac17d0b598978ed7b580ee
yankangjie <287207951@qq.com>
2020-07-15 11:17:25 +0800

    Update .feflowrc.js

--- vueDemo/.feflowrc.js
+++ vueDemo/.feflowrc.js
@@ -5,39 +5,37 @@ module.exports = {
                 builder: "feflow-devkit-xbc-webpack4:dev",
                 options: {
                     isMinicss: false,
-					port: 1123
+                    port: 1123
                 },
             },
             test: {
                 builder: "feflow-devkit-xbc-webpack4:test",
                 options: {
-					isMinicss: true,
+                    isMinicss: true,
                 }
             },
             formalTest: {
                 builder: "feflow-devkit-xbc-webpack4:formalTest",
                 options: {
-					isMinicss: true,
+                    isMinicss: true,
                 }
             },
             demo: {
                 builder: "feflow-devkit-xbc-webpack4:demo",
                 options: {
-					isMinicss: true,
+                    isMinicss: true,
                 }
             },
             build: {
                 builder: "feflow-devkit-xbc-webpack4:build",
                 options: {
-					isMinicss: true,
-					externals: [
-						{
-							optionsId: 100,
-							module: "vue",
-							entry: "../../static/js/vue2.5.2.min.js",
-							global: "Vue"
-						},
-					],
+                    isMinicss: true,
+                    externals: [{
+                        optionsId: 100,
+                        module: "vue",
+                        entry: "../../static/js/vue2.5.2.min.js",
+                        global: "Vue"
+                    }, ],
                 }
             }
         },
@@ -49,23 +47,20 @@ module.exports = {
             analyzer: {
                 "analyzerPort": 3479
             },
-			port: 1234,
-			alias: {
-				"@": "src",
-			},
-            externals: [
-				{
-					optionsId: 100,
-                    module: "vue",
-                    entry: "../../static/js/vue2.5.2.js",
-                    global: "Vue"
-				},
-				{
-                    module: "element-ui",
-                    entry: "../../static/js/element-ui2.2.1.min.js",
-                    global: "ELEMENT"
-                },
-			],
+            port: 1234,
+            alias: {
+                "@": "src",
+            },
+            externals: [{
+                optionsId: 100,
+                module: "vue",
+                entry: "../../static/js/vue2.5.2.js",
+                global: "Vue"
+            }, {
+                module: "element-ui",
+                entry: "../../static/js/element-ui2.2.1.min.js",
+                global: "ELEMENT"
+            }, ],
             envs: {
                 dev: {
                     envObj: {
@@ -76,25 +71,25 @@ module.exports = {
                 test: {
                     envObj: {
                         NODE_ENV: "'test'",
-						API_HOST: "'http://x.x.x.x:xxxx'",
+                        API_HOST: "'http://x.x.x.x:xxxx'",
                     }
                 },
                 formalTest: {
                     envObj: {
                         NODE_ENV: "'formalTest'",
-						API_HOST: "'http://x.x.x.x:xxxx'",
+                        API_HOST: "'http://x.x.x.x:xxxx'",
                     }
                 },
                 demo: {
                     envObj: {
                         NODE_ENV: "'demo'",
-						API_HOST: "'http://x.x.x.x:xxxx'",
+                        API_HOST: "'http://x.x.x.x:xxxx'",
                     }
                 },
                 build: {
                     envObj: {
                         NODE_ENV: "'production'",
-						API_HOST: "'http://x.x.x.x:xxxx'",
+                        API_HOST: "'http://x.x.x.x:xxxx'",
                     }
                 }
             }
